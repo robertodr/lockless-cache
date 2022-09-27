fdbff630f3337f2b2e1ef2de434a4e3f9ef66139
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-09-27 17:35:31 +0200

    Add new silly file

--- lockless_cache/__init__.py
+++ lockless_cache/__init__.py
@@ -1,5 +1,6 @@
 __version__ = "0.1.0"
 
+from lockless_cache.multiplier import multiply_two_ints
 from lockless_cache.summer import sum_two_ints
 
-__all__ = ["sum_two_ints"]
+__all__ = ["sum_two_ints", "multiply_two_ints"]
