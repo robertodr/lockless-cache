a7becc5351b2eaf500b44c6c0fd223de706c3f3a
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-09-28 11:30:38 +0200

    Silly commit

--- tests/test_lockless_cache.py
+++ tests/test_lockless_cache.py
@@ -7,14 +7,6 @@ def test_version():
     assert __version__ == "0.1.0"
 
 
-def test_foo():
-    assert 3 == (1 + 2)
-
-
-def test_bar():
-    assert 2 == (1 + 1)
-
-
 @pytest.mark.parametrize(
     "expected,lhs,rhs",
     [
